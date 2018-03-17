d3b747442972f4be99ab901039651ce57ed8d9c5
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-17 19:34:44 +0100

    It's cleaner now!! :)

--- Applicatives.playground/Contents.swift
+++ Applicatives.playground/Contents.swift
@@ -1,10 +1,13 @@
 //: Playground - Applicatives
 
 func createUser(name: String, password: String, premium: Bool, newsletter: Bool) -> Result<User, UserError> {
-
-    let result: Result<User, UserError> = Result.pure(newsletter).apply(Result.pure(premium).apply(Validators.Password(password).apply(Validators.Name(name).map(curry(User.init)))))
+    let result: Result<User, UserError> = Result.pure(newsletter)
+        .apply(Result.pure(premium)
+            .apply(Validators.Password(password)
+                .apply(Validators.Name(name)
+                    .map(curry(User.init)))))
     
-    return result.flatMap(UserValidator.Premium || UserValidator.Newsletter)
+    return result.flatMap(UserValidator.Newsletter || UserValidator.Premium)
 }
 
 let user = createUser(name: "alex", password: "functionalswift", premium: true, newsletter: false)
