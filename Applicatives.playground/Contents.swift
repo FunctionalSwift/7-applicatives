ad5c838ac6120b30ba5ce22cfdeb48a099f8ceaf
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-17 18:45:31 +0100

    Swifting our pseudocode

--- Applicatives.playground/Contents.swift
+++ Applicatives.playground/Contents.swift
@@ -1,13 +1,13 @@
 //: Playground - Applicatives
 
 func createUser(name: String, password: String, premium: Bool, newsletter: Bool) -> Result<User, UserError> {
-    // TRATA DE CREAR UN USUARIO, UTILIZANDO:
+    // let result = TRATA DE CREAR UN USUARIO, UTILIZANDO:
     // CÓMO name,       EL RESULTADO DE VALIDAR "name"
     // CÓMO password,   EL RESULTADO DE VALIDAR "password"
     // CÓMO premium,    EL RESULTADO DE VALIDAR "premium"
     // CÓMO newsletter, EL RESULTADO DE VALIDAR "newsletter"
     
-    // SI LA CREACIÓN DEL USER HA FALLADO, RETORNA EL FALLO ó SINO, RETORNA EL USUARIO
+    // return SI LA CREACIÓN DEL USER HA FALLADO, RETORNA EL FALLO ó SINO, RETORNA EL USUARIO
 }
 
 let user = createUser(name: "alex", password: "functionalswift", premium: true, newsletter: false)
